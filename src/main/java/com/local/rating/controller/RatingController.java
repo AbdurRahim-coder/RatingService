8aeabcd41ce03fdf32681d41e5f672bed5a68b75
abdurrahim <abdur.rahim@tallykhata.com>
2023-03-29 10:49:40 +0600

    add restTemplete

--- src/main/java/com/local/rating/controller/RatingController.java
+++ src/main/java/com/local/rating/controller/RatingController.java
@@ -16,7 +16,6 @@ import java.util.List;
 public class RatingController {
 
     private RatingService ratingService;
-    private final RestTemplate restTemplate;
 
 
 //    public RatingController(RatingService ratingService) {
